46ed9dbf9f4ff89f48d685248893fd479864e667
Ross Large <ross_large@msn.com>
2026-04-09 23:19:43 +0100

    Fix file path for referencing src location

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': '/Users/ross/Developer/Code/kanban/src',
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
